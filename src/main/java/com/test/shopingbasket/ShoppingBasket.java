99376c5e6bac68d72cfb2002fa67556938e9e274
Touqir Abbas <tauqeer555pk@yahoo.com>
2016-04-21 12:37:54 +0100

    passed the failing test

--- src/main/java/com/test/shopingbasket/ShoppingBasket.java
+++ src/main/java/com/test/shopingbasket/ShoppingBasket.java
@@ -3,7 +3,17 @@ package com.test.shopingbasket;
 import java.math.BigDecimal;
 
 public class ShoppingBasket {
-    public BigDecimal calculatePrice(String... item) {
-        return null;
+    public BigDecimal calculatePrice(String... items) {
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        for(String item : items ) {
+
+            switch(item) {
+                case "Apple":
+                    totalPrice = totalPrice.add(new BigDecimal("0.60"));
+                    break;
+            }
+        }
+
+        return totalPrice;
     }
 }
